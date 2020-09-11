04f2860ec833c8aaa4a6a1204bdc403f8ce39dda
rafasuman <rleonardomartinez@unicesar.edu.co>
2020-09-11 14:00:07 -0500

    first commit

---
+++ ejercicio1/Aplicacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio1
+{
+    class Aplicacion
+    {
+        public Aplicacion()
+        {
+            RecibirPromedio();
+        }
+        public Promedio ObtenerPromedio()
+        {
+            Console.WriteLine("Digite la nota 1");
+            float nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la nota 2");
+            float nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la nota 3");
+            float nota3 = float.Parse(Console.ReadLine());
+            Promedio promedio = new Promedio(nota1,nota2,nota3);
+            return promedio;
+        }
+
+        public void RecibirPromedio()
+        {
+            Promedio promedio = ObtenerPromedio();
+            string mensaje = promedio.ObtenerResultadoDelPromedio();
+            Console.WriteLine(mensaje);
+        }
+    }
+}
---
+++ ejercicio1/promedio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio1
+{
+    class Promedio
+    {
+        public float Nota1 { get; set; }
+        public float Nota2 { get; set; }
+        public float Nota3 { get; set; }
+
+        public Promedio(float nota1,float nota2,float nota3)
+        {
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+          
+
+        public float ObtenerPromedio()
+        {
+           float resultado = (Nota1 + Nota2 + Nota3) / 3;
+            return resultado;
+        }
+
+        public string ObtenerResultadoDelPromedio()
+        {
+            float resultado = ObtenerPromedio();
+            
+            if(resultado>=3)
+            {
+                return "el estudiante aprueba";
+            }
+            else
+            {
+                return "el estudiante reprueba";
+            }
+        }
+
+    }
+}
